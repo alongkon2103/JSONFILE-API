c0e29987417db7ea24a597d0a0cf8fa02bf450c1
alongkon2103 <93041228+alongkon2103@users.noreply.github.com>
2023-09-08 22:38:09 +0700

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,8 @@ const productsFilePath = path.join(__dirname, 'productsjson.json');
 
 app.get('/', (req, res) => {
   // res.sendFile(path.join(__dirname, 'public', 'index.html'));
-  res.send(`<!doctype html>
+  res.send(`
+  <!doctype html>
 <html lang="en">
   <head>
     <meta charset="utf-8">
@@ -25,14 +26,15 @@ app.get('/', (req, res) => {
     <div class="container mt-5">
         <ul class="list-group">
             <li class="list-group-item bg-dark text-white"><h3>API ANIME PRODUCT <span class="" style="font-size: 15px;">By : alongkon</span></h3></li>
-            <li class="list-group-item list-group-item-dark">For get all product : /product</li>
-            <li class="list-group-item list-group-item-dark">For get 1 product with id : /product/1</li>
-            <li class="list-group-item list-group-item-dark">For search item in product : /product/search?q={search}</li>
+            <li class="list-group-item list-group-item-dark">For get all product :  <a class="link-opacity-75-hover" href="https://alongkonproductapi.vercel.app/product">https://alongkonproductapi.vercel.app/product</a> </li>
+            <li class="list-group-item list-group-item-dark">For get 1 product with id : <a class="link-opacity-75-hover" href="https://alongkonproductapi.vercel.app/product/1">https://alongkonproductapi.vercel.app/product/1</a></li>
+            <li class="list-group-item list-group-item-dark">For search item in product : <a class="link-opacity-75-hover" href="https://alongkonproductapi.vercel.app/products/search?q=kagu">https://alongkonproductapi.vercel.app/products/search?q=kagu</a></li>
           </ul>
     </div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
   </body>
-</html>`)
+</html>
+  `)
 });
 
 app.get('/products', (req, res) => {
@@ -78,7 +80,34 @@ app.post('/submit', (req, res) => {
   }
 });
 app.get('/addproduct', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'add_data.html'));
+  res.send(`
+  <!DOCTYPE html>
+<html lang="en">
+<head>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Data Collection Form</title>
+</head>
+<body>
+
+  <form action="/submit" method="post" class="container">
+    <h1>เพิ่มข้อมูลใน API</h1>
+    <label for="id">ID:</label>
+    <input type="text" id="id" name="id" required class="form-control shadow-none"><br>
+    <label for="title">Title:</label>
+    <input type="text" class="form-control shadow-none" id="title" name="title" required><br>
+    <label for="image">Image:</label>
+    <input type="text" class="form-control shadow-none" id="image" name="image" required><br>
+    <label for="studios">Studios:</label>
+    <input type="text" class="form-control shadow-none" id="studios" name="studios" required><br>
+    <button type="submit" class="btn btn-success">Submit</button>
+  </form>
+</body>
+</html>
+
+  
+  `)
 });
 
 app.get('/products/:id', (req, res) => {
